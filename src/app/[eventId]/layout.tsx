d0266bc7b6d6da606c88c0cb48247ce6574f068a
Jpuc1143 <jaime.perez@uc.cl>
2024-11-17 14:59:17 -0300

    Feat: add platanus logo

--- src/app/[eventId]/layout.tsx
+++ src/app/[eventId]/layout.tsx
@@ -1,5 +1,7 @@
 import Link from 'next/link'
 import { ReactNode } from 'react'
+import platanusLogoHorizontal from './platanus-logo-horizontal.svg'
+import Image from 'next/image'
 
 export default async function EventLayout({
   params,
@@ -12,9 +14,9 @@ export default async function EventLayout({
 
   return (
     <>
-      <header className="m-3 items-center self-stretch sm:grid">
-        <Link href="https://platan.us" className="justify-self-start text-xl">
-          Platanus
+      <header className="m-3 grid items-center self-stretch">
+        <Link href="https://platan.us" className="justify-self-start">
+          <Image src={platanusLogoHorizontal} alt="Platanus" height={30} />
         </Link>
         <h1 className="justify-self-center font-mono text-4xl">
           Hackathon {eventId}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,8 +13,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: 'Next.js and Supabase Starter Kit',
-  description: 'The fastest way to build apps with Next.js and Supabase',
+  title: 'platanus hack',
+  description: 'platanus hack',
 }
 
 const oxanium = Oxanium({
